3a3d51c3d2ed31956e22591c8d86f24127133c47
Jade Huang <jade.huang1@ucalgary.ca>
2026-04-08 00:23:49 -0600

    bugfix

--- src/main/java/com/jade/dreamjournalv2/DatabaseManager.java
+++ src/main/java/com/jade/dreamjournalv2/DatabaseManager.java
@@ -28,7 +28,7 @@ public class DatabaseManager {
                 + " id INTEGER PRIMARY KEY AUTOINCREMENT,\n" // Gives every dream a unique number
                 + " content TEXT NOT NULL,\n"               // Where the encrypted dream goes
                 + " is_lucid BOOLEAN NOT NULL,\n"            // True/False for your analytics
-                + " date_logged DATETIME DEFAULT CURRENT_TIMESTAMP\n" // Auto-stamps the date
+                + " date_logged DATETIME DEFAULT CURRENT_TIMESTAMP,\n" // Auto-stamps the date
                 + " hours_slept INTEGER,\n" // Track how many hours I slept
                 + " mood_before_sleep TEXT,\n" // Track my mood before sleeping
                 + " mood_after_sleep TEXT,\n" // Track mood after waking up
